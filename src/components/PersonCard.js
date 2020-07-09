8963a386324423ad640a5cd19edb045e41a6ab6c
Jason Brady <jasonmbrady42@gmail.com>
2020-07-09 08:29:07 -0700

    completed Big Inversion assignment

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,25 +1,12 @@
 import React, { Component } from "react"
 
-class PersonCard extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            age: this.props.age
-        }
-    }
-    render() {
-        const { firstName, lastName, hairColor } = this.props;
-        const age = this.state.age;
-        return (
-            <div>
-                <h1>{lastName}, {firstName}</h1>
-                <h3>Age: {age}</h3>
-                <h3>Hair Color: {hairColor}</h3>
-                <button onClick={() => { this.setState(state => ({ age: age + 1 })) }}>
-                    Birthday button for {firstName} {lastName}
-                </button>
-            </div >
-        );
-    }
+const PersonCard = (props) => {
+    return (
+        <div>
+            <h1>{props.lastName}, {props.firstName}</h1>
+            <h3>Age: {props.age}</h3>
+            <h3>Hair Color: {props.hairColor}</h3>
+        </div>
+    );
 }
 export default PersonCard;
